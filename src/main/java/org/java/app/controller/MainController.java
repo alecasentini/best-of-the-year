3274259c1fb0e6f0d8102afd6b834e4af9809f44
alecasentini <ale.casentini@gmail.com>
2023-10-02 14:53:53 +0200

    Step 2

--- src/main/java/org/java/app/controller/MainController.java
+++ src/main/java/org/java/app/controller/MainController.java
@@ -1,5 +1,11 @@
 package org.java.app.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.java.app.movie.Movie;
+import org.java.app.song.Song;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +19,42 @@ public class MainController {
 		model.addAttribute("name",name);
 		
 		return "index";
+	}
+	
+	private List<Movie> getBestMovies() {
+		 List<Movie> movies = new ArrayList<>();
+		    movies.add(new Movie(1, "Jurassic Park"));
+		    movies.add(new Movie(2, "Titanic"));
+		    movies.add(new Movie(3, "Avatar"));
+		    movies.add(new Movie(4, "E.T."));
+		    movies.add(new Movie(5, "Lo squalo"));
+		    return movies;
+	}
+	
+	private List<Song> getBestSongs() {
+	    List<Song> songs = new ArrayList<>();
+		songs.add(new Song(1, "Thriller"));
+	    songs.add(new Song(2, "One More Time"));
+	    songs.add(new Song(3, "Master of Puppet"));
+	    songs.add(new Song(4, "2 Minutes to Midnight"));
+	    songs.add(new Song(5, "Smells Like Teen Spirit"));
+		return songs;
+	}
+	
+	@GetMapping("/movies")
+	public String movies(Model model){
+		List<Movie> bestMovies = getBestMovies();
+		String movieList = bestMovies.stream().map(Movie::getTitle).collect(Collectors.joining(", "));
+		model.addAttribute("movieList", movieList);
+		return "movies";
+	}
+	
+	@GetMapping("/songs")
+	public String songs(Model model){
+		List<Song> bestSongs = getBestSongs();
+		String songList = bestSongs.stream().map(Song::getTitle).collect(Collectors.joining(", "));
+		model.addAttribute("songList", songList);
+		return "songs";
 	}
 
 }
